94e2cbfe218294c41508b14be25eeb98ebe5ba98
User <vuyanidick620@gmail.com>
2022-06-06 10:45:47 +0200

    THE HOMEPAGE IS GONE ADDED A SPLASH SCREEN

--- app/src/main/java/com/example/mccourseforum/MainActivity.java
+++ app/src/main/java/com/example/mccourseforum/MainActivity.java
@@ -4,32 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int SPLASH = 3500;
 
+    Animation top_animation,bottom_animation;
+    ImageView logo;
+    TextView title,tagline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        top_animation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottom_animation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        tagline = findViewById(R.id.tagline);
 
+        logo.setAnimation(top_animation);
+        title.setAnimation(bottom_animation);
+        tagline.setAnimation(bottom_animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH);
+
+    }
 //        Bundle bundle = getIntent().getExtras();
 //        String loggedIn = bundle.getString("loggedUser");
 //        System.out.println(loggedIn);
-    }
-    public void goS(View view) {
-        Intent intent = new Intent(this , Signup.class );
-        startActivity(intent);
-    }
-
-    public void goL(View view) {
-        Intent intent = new Intent(this , Login.class );
-        startActivity(intent);
-    }
-    public void goQ (View view){
-        Intent i = new Intent(this,QuestionsDisplay.class);
-        startActivity(i);
-    }
 
 
 }
